05ad783ec45b79601aa839f99a5f1d8895a57b99
Kawshik Biswas <kawshikbss@gmail.com>
2021-12-08 09:19:21 +0600

    configured db in __init__

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,11 @@
-from flask import Flask
+from flask import Flask, Blueprint
+from flask_login import login_manager
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+
+
+DB_NAME = 'database.db'
+db = SQLAlchemy()
 
 
 def create_app(config_filename):
@@ -6,5 +13,12 @@ def create_app(config_filename):
     
     app.config.from_pyfile(config_filename)
 
+    db.init_app(app=app)
+    create_db(app=app)
+
     return app
 
+
+def create_db(app):
+    if not path.exists(DB_NAME):
+        db.create_all(app=app)
