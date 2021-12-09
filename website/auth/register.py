9d11016a6084048afefe7e8a7242f23758549c1d
Kawshik Biswas <kawshikbss@gmail.com>
2021-12-09 16:46:51 +0600

    completed registration page and retriving data from form in backend

--- website/auth/register.py
+++ website/auth/register.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from flask.templating import render_template
 
 
 register = Blueprint('register', __name__)
@@ -6,4 +7,11 @@ register = Blueprint('register', __name__)
 
 @register.route('/register/', methods=['GET', 'POST'])
 def registration():
-    return '<h1>REg</h1>'
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        print(first_name + last_name + email + password + confirm_password)
+    return render_template('register.html')
