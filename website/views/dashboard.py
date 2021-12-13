ba24b0f6b99335dfd70bf6c417b4b7c56d3c2550
Kawshik Biswas <kawshikbss@gmail.com>
2021-12-13 16:08:59 +0600

    created dashboard route and page and added tasks list to dashboard

--- website/views/dashboard.py
+++ website/views/dashboard.py
@@ -1,11 +1,18 @@
-from flask import Blueprint, request, render_template, flash
+from flask import Blueprint, request, render_template, flash, json, jsonify
 from flask_login import current_user, login_required
 
 
 dashboard = Blueprint('dashboard', __name__)
 
-@login_required
 @dashboard.route('/dashboard/', methods=['POST', 'GET'])
+@login_required
 def dashboard_page():
 
-    return render_template('dashboard.html', user=current_user)
+    return render_template('dashboard.html', user=current_user, time='9:00 AM')
+
+@dashboard.route('/manage_task/', methods=['POST'])
+@login_required
+def manage_task():
+    task_id = json.loads(request.data)['taskId']
+    print(task_id)
+    return jsonify({})
